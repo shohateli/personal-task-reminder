f7177e3daf711dd6c0afd07e92434c84c573bfb7
Eli <eashohat@gmail.com>
2021-02-23 19:25:51 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,167 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import { Button, StyleSheet, Text, TextInput, View, Platform, FlatList, Dimensions, } from 'react-native';
+import DialogInput from 'react-native-dialog-input';
+import AsyncStorage from '@react-native-community/async-storage';
+import { render } from 'react-dom';
+
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+
+// import NotifyMe from './notification';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+function isNumber(str) {
+  if (typeof str != "string") return false // we only process strings!
+  // could also coerce to string: str = ""+str
+  return !isNaN(str) && !isNaN(parseFloat(str))
+}
 
 export default function App() {
+  const [listOfChores, setListOfChores]= useState([]);
+  const [yesNo, setYesNo] = useState('yes');
+  const [whateverInput, setWhateverInput] = useState();
+  const [isDialogVisible, setIsDialogVisible] = useState(false);
+  const [isManyHoursVisible, setIsManyHoursVisible] = useState(false);
+  const [currentlyChanged, setCurrentlyChanged] = useState("qwertyasdfzxcv");
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
+  
+  Date.prototype.addHours= function(h){
+    this.setHours(this.getHours()+h);
+    return this;
+  }
+
+  const addHourData = async (inputText) => {
+    console.log(currentlyChanged);
+    if(inputText !== "nope"){
+      if(isNumber(inputText) == true) {
+        await schedulePushNotification(parseInt(inputText));
+      }
+    } else {
+      console.log('nope');
+    }
+    await setCurrentlyChanged('qwertyasdfzxcv');
+    console.log(currentlyChanged);
+    setIsManyHoursVisible(false);
+  }
+
+  useEffect(()=> {
+    async function fetchData() {
+      let value = await AsyncStorage.getItem('arrayOfMessages');
+      let data = JSON.parse(value);
+      setListOfChores(data);
+      console.log(data);
+    }
+    fetchData();
+  }, [yesNo]);
+
+  const pressPlus = () => {
+    setIsDialogVisible(true);
+  }
+
+  const addTheData = async (inputText) => {
+    let currentInput = listOfChores;
+    currentInput.push(inputText);
+    //console.log(typeof currentInput);
+    //console.log(JSON.stringify(currentInput));
+    await AsyncStorage.setItem('arrayOfMessages', JSON.stringify(currentInput));
+    let yes_or_no = yesNo;
+    if (yes_or_no === 'yes'){
+      setYesNo('no');
+    } else {
+      setYesNo('yes');
+    }
+    //console.log(currentInput);
+    //console.log(currentInput.length);
+    setIsDialogVisible(false);
+    console.log(currentlyChanged);
+    await setCurrentlyChanged(inputText);
+    console.log(currentlyChanged);
+    setIsManyHoursVisible(true);
+    // alert(new Date().addHours(4));
+  }
+
+  const removeFromData = async (itemToRemove) => {
+    console.log('starting process');
+    let copyOfList = listOfChores;
+    var indexOfItem = copyOfList.indexOf(itemToRemove);
+    if (indexOfItem > -1) {
+      copyOfList.splice(indexOfItem, 1);
+    }
+    await AsyncStorage.setItem('arrayOfMessages', JSON.stringify(copyOfList));
+    let yes_or_no = yesNo;
+    if (yes_or_no === 'yes'){
+      setYesNo('no');
+    } else {
+      setYesNo('yes');
+    }
+    console.log('ending process');
+  }
+
+  const renderItemComponent = ({ item }) => {
+    return(
+      <View style={styles.listItem}>
+        <Text style={{fontWeight: '700', fontSize: 15, flexShrink: 2, width: '90%'}}>{item}</Text>
+        <View style={{alignItems: 'flex-start', flex:1, flexDirection: 'row-reverse', marginLeft: 10, }}>
+          <Text style={{fontWeight: '700', fontSize: 20,}} onPress={()=>{removeFromData(item)}} >
+            -
+          </Text>
+        </View>
+      </View>
+    );
+  }
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <NotifyMe /> */}
+      <DialogInput isDialogVisible={isDialogVisible}
+            title={"Add Task"}
+            message={"New Task"}
+            hintInput ={"Enter The Task You Want To Add"}
+            submitInput={(inputText) => {addTheData(inputText)}}
+            closeDialog={ () => {setIsDialogVisible(false)}}>
+      </DialogInput>
+      <DialogInput isDialogVisible={isManyHoursVisible}
+            title={"Hours"}
+            message={"Ammount of Hours To Be Reminded In"}
+            hintInput ={"ONLY ENTER A NUMBER!!!!!!"}
+            submitInput={(inputText) => {addHourData(inputText)}}
+            closeDialog={ () => {addHourData("nope")}}>
+      </DialogInput>
+      <FlatList 
+        style={styles.showList}
+        data={listOfChores}
+        renderItem={renderItemComponent}
+      />
+      <Text style={styles.plus} onPress={pressPlus}>+</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +170,81 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9532a8',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  plus: {
+    position: 'absolute',
+    bottom: 3,
+    right: 10,
+    fontSize: 60,
+    borderWidth: 3,
+    paddingLeft: 27,
+    paddingRight: 22,
+    borderRadius: 50,
+  },
+  showList: {
+    marginTop: 50,
+    width: '95%',
+    borderRadius: 25,
+    backgroundColor: '#b950d4',
+    flexGrow: 0
+  }, 
+  listItem: {
+    flex:1,
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    borderRadius: 25
+  } 
 });
+
+
+async function schedulePushNotification(numHours) {
+  let injectHour;
+  if(numHours == 0){
+    injectHour = 1;
+  } else {
+    injectHour = Math.round(numHours) * 60 *60;
+  }
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Reminder",
+      body: `Go do whatever!!!`,
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: injectHour },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
---
+++ notification.js
@@ -0,0 +1,90 @@
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+export default function NotifyMe() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
+
+  return (
+    <View style={{marginTop: 50}}>
+      {/* <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      /> */}
+      <Text>nothing for now</Text>
+    </View>
+  );
+}
+
+async function schedulePushNotification(numHours) {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Reminder",
+      body: 'Go do {reminder}!!!',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: numHours },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
